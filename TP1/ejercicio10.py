ffe07d95fd4f5e84ee5857d438b1039951d0e99f
Perez Zavaleta Ramiro <ramirospz@outlook.com>
2022-03-28 19:14:49 -0300

    prueba

---
+++ TP1/ejercicio10.py
@@ -0,0 +1,18 @@
+from random import random
+
+
+print("Adivine el numero")
+numero=random[0,100]
+ingreso=0
+vidas=5
+
+while vidas!=0:
+    ingreso=float(input(f"Ingrese un numero--- tiene {vidas} vidas"))
+    if ingreso==numero:
+        print("Usted a adivinado el numero")
+        break
+    else:
+        vidas=vidas-1
+
+if vidas==0:
+    print("Usted a perdido u.u")
---
+++ TP1/ejercicio9.py
@@ -0,0 +1,26 @@
+#calculadora simple
+from typing import no_type_check
+
+
+operaciones=input("Seleccione 1 para sumar, 2 para restar, 3 para multiplicar y 4 para dividir\n")
+i=0
+resultado=0
+numeros=[]
+print("ingrese los numero a operar")
+for i in range(0,2):
+    numeros.append(float(input()))
+    i=i+1
+
+
+if operaciones==1:
+    resultado=numeros[0]+numeros[1]
+    print(f"El resultado es {resultado}")
+elif operaciones==2:
+    resultado= numeros[0]-numeros[1]
+    print(f"El resultado es {resultado}")
+elif operaciones==3:
+    resultado=numeros[0]*numeros[1]
+    print(f"El resultado es {resultado}")
+elif operaciones==4:
+    resultado=numeros[0]/numeros[1]
+    print(f"El resultado es {resultado}")
